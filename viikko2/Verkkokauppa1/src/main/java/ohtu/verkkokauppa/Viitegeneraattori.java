bd5d40dac105c66bd1ba07218ff9b3445cc9a054
bensku <bmi@iki.fi>
2019-11-09 16:41:58 +0200

    More decoupling

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -1,20 +1,10 @@
 package ohtu.verkkokauppa;
 
 public class Viitegeneraattori implements IViitegeneraattori {
-
-    private static IViitegeneraattori instanssi;
-
-    public static IViitegeneraattori getInstance() {
-        if (instanssi == null) {
-            instanssi = new Viitegeneraattori();
-        }
-
-        return instanssi;
-    }
     
     private int seuraava;
     
-    private Viitegeneraattori(){
+    public Viitegeneraattori(){
         seuraava = 1;    
     }
     
