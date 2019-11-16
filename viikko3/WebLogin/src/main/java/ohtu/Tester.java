d12bfac10f0cdd3db5ce8d5ba5e62168fb270a91
bensku <bmi@iki.fi>
2019-11-16 22:55:13 +0200

    Selenium testing

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -5,28 +5,59 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.github.bonigarcia.wdm.DriverManagerType;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Tester {
 
     public static void main(String[] args) {
+    	WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
         
         sleep(2);
         
-        WebElement element = driver.findElement(By.linkText("login"));
+//        WebElement element = driver.findElement(By.linkText("login"));
+//        element.click();
+//
+//        sleep(2);
+//
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akkep");
+//        element = driver.findElement(By.name("login"));
+        
+//        element = driver.findElement(By.name("username"));
+//        element.sendKeys("pekka");
+//        element = driver.findElement(By.name("password"));
+//        element.sendKeys("akke");
+//        element = driver.findElement(By.name("login"));
+        
+        WebElement element = driver.findElement(By.linkText("register new user"));
         element.click();
-
+        
         sleep(2);
-
+        
         element = driver.findElement(By.name("username"));
-        element.sendKeys("pekka");
+        element.sendKeys("test");
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
-        element = driver.findElement(By.name("login"));
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("signup"));
         
         sleep(2);
         element.submit();
+        
+        sleep(2);
+        
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+        
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
 
         sleep(3);
         
