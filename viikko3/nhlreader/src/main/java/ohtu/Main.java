7637f095a2e5318b33adee24f808ac83365f937c
bensku <bmi@iki.fi>
2019-11-16 21:59:37 +0200

    Do 3.1

--- viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ohtu;
 
-/**
- *
- * @author mluukkai
- */
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Request;
+
+import com.google.gson.Gson;
+
 public class Main {
-    
+	
+	public static void main(String... args) throws ClientProtocolException, IOException {
+		String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+		
+		String bodyText = Request.Get(url).execute().returnContent().asString();
+		        
+		System.out.println("json-muotoinen data:");
+		System.out.println( bodyText );
+		
+		Gson mapper = new Gson();
+		Player[] players = mapper.fromJson(bodyText, Player[].class);
+		
+		System.out.println("Oliot:");
+		for (Player player : players) {
+		    System.out.println(player);
+		}
+	}
 }
--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -2,19 +2,58 @@
 package ohtu;
 
 public class Player {
-    private String name;
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
-      
+	
+	private String name;
+	private String birthdate;
+	private String team;
+	private String nationality;
+    
+    private int goals;
+    private int assists;
+    private int penalties;
+    
+    // GSON can skip this with sun.misc.Unsafe
+	public Player(String name, String birthdate, String team, String nationality, int goals, int assists,
+			int penalties) {
+		this.name = name;
+		this.birthdate = birthdate;
+		this.team = team;
+		this.nationality = nationality;
+		this.goals = goals;
+		this.assists = assists;
+		this.penalties = penalties;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getBirthdate() {
+		return birthdate;
+	}
+	
+	public String getTeam() {
+		return team;
+	}
+	
+	public String getNationality() {
+		return nationality;
+	}
+	
+	public int getGoals() {
+		return goals;
+	}
+	
+	public int getAssists() {
+		return assists;
+	}
+	
+	public int getPenalties() {
+		return penalties;
+	}
+	
+	@Override
+	public String toString() {
+		return name + " team " + team + " goals " + goals + " assists " + assists;
+	}
 }
