b23554a302eac87b8041422891f7132ddb0ae0b5
bensku <bmi@iki.fi>
2019-11-20 19:03:38 +0200

    Refactor worst out of TennisGame

--- viikko4/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,80 +1,62 @@
 package ohtu;
 
 public class TennisGame {
+	
+	private static class Player {
+		
+		public final String name;
+		public int score;
+		
+		public Player(String name) {
+			this.name = name;
+			this.score = 0;
+		}
+	}
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private final Player[] players;
 
     public TennisGame(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.players = new Player[] {new Player(player1Name), new Player(player2Name)};
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        for (Player p : players) {
+        	if (p.name.equals(playerName)) {
+        		p.score++;
+        	}
+        }
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+        int score1 = players[0].score;
+        int score2 = players[1].score;
+        
+        // Both players have equal score
+        if (score1 == score2) {
+            return scoreToString(score1) + "-All";
+        } else if (score1 >= 4 || score2 >= 4) {
+            int diff = score1 - score2;
+            if (diff == 1) return "Advantage player1";
+            else if (diff == -1) return "Advantage player2";
+            else if (diff >= 2) return "Win for player1";
+            else return "Win for player2";
+        } else {
+        	return scoreToString(score1) + "-" + scoreToString(score2);
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
-        return score;
+    }
+    
+    private String scoreToString(int score) {
+    	switch (score) {
+	        case 0:
+	            return "Love";
+	        case 1:
+	            return "Fifteen";
+	        case 2:
+	            return "Thirty";
+	        case 3:
+	            return "Forty";
+	        default:
+	            return "Deuce";
+    	}
     }
 }
